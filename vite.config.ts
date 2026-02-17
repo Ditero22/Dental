1c0d591508ce3c784d588e007d2f32d8b99a903c
Karl <karl.ortega.2002.ko@gmail.com>
2026-02-17 20:48:26 +0800

    Remove alias configuration from vite.config.ts
    Removed alias configuration from Vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,4 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   base:"/Dental",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 })
