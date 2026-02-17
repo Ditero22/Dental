558aaebcdc938068cfc500956f7fedcd9ab70a18
Ditero22 <karl.ortega.2002.ko@gmail.com>
2026-02-17 18:56:18 +0800

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
-import mdx from '@mdx-js/rollup'
 import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    { enforce: 'pre', ...mdx() },
-    react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),tailwindcss()],
+  plugins: [react(),tailwindcss()],
+  base:"/Dental",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
