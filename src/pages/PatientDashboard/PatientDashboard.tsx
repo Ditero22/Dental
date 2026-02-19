ddf534d73908a2817a2d7500866c437bc160e720
Ditero22 <karl.ortega.2002.ko@gmail.com>
2026-02-19 20:25:30 +0800

    Update Patient Dashboard

--- src/pages/PatientDashboard/PatientDashboard.tsx
+++ src/pages/PatientDashboard/PatientDashboard.tsx
@@ -2,9 +2,7 @@ import { Navbar } from "@/components";
 import { useAuth } from "@/context";
 import { Outlet } from "react-router-dom";
 import { AppointmentRecord } from "./AppointmentRecord";
-import { Calendar } from "./AppointmentCalendarT";
-
-
+import { AppointmentCalendar } from "./AppointmentCalendar";
 
 export function PatientDashboard() {
   const { loggedUser } = useAuth();
@@ -15,20 +13,19 @@ export function PatientDashboard() {
       <nav className="sticky top-0 z-50 bg-white shadow-md flex items-center px-6">
         <Navbar mode="dashboard" userName={userName} />
       </nav>
-
-      <main className="flex-1 p-6 mt-15">
-        <div className="flex flex-col md:flex-row gap-6">
-          <div className="flex-1">
+      <main className="flex-1 p-4 sm:p-6 mt-15">
+        <div className="flex flex-col-reverse md:flex-row gap-6">
+          <div className="w-full md:flex-1">
             <AppointmentRecord />
           </div>
-          <div className="flex-2">
-            <div className="bg-white rounded-xl shadow p-4">
-              <Calendar />
-            </div>
+          <div className="w-full md:flex-[2]">
+            <AppointmentCalendar />
           </div>
 
-        </div> 
+        </div>
+
         <Outlet />
+
       </main>
     </div>
   );
