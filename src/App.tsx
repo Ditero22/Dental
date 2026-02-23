a01c83986675a43c0ec2ab9cdc381e15a72f1151
croxcut <johnpaulvalenzuelaog@gmai.com>
2026-02-23 20:08:30 +0800

    add: init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
