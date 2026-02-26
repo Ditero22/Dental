a77c508c996879b6d01bf04a2d78574415990e94
Ditero22 <karl.ortega.2002.ko@gmail.com>
2026-02-26 18:25:39 +0800

    Update Navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,13 +1,5 @@
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { useState, useEffect, useRef } from "react";
-<<<<<<< HEAD
-import { LoginModal, ForgotPassword } from "./modal";
-import { ModalImg } from "@/assets";
-import { Bell, User } from "lucide-react";
-import { useAuth } from "@/context";
-import { MessageIcon } from "@/assets";
-import { ConfirmModal } from "./modal";
-=======
 import { LoginModal, ForgotPassword } from "../modal";
 import { ModalImg, MessageIcon } from "@/assets";
 import { Bell } from "lucide-react";
@@ -15,7 +7,6 @@ import { useAuth } from "@/context";
 import { cn } from '@/lib';
 import { MessageDropdown, UserDropdown, NotifDropdown } from "../ui";
 import { conversations, notifications } from "@/types";
->>>>>>> main
 
 type NavbarProps = {
   className?: string;
@@ -33,9 +24,6 @@ export function Navbar({ className, mode, userName, openLogin }: NavbarProps) {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const [isLoginOpen, setIsLoginOpen] = useState(false);
   const [isForgotOpen, setIsForgotOpen] = useState(false);
-<<<<<<< HEAD
-  const [confirmLogout, setConfirmLogout] = useState(false);
-=======
   const [isMessageOpen, setIsMessageOpen] = useState(false);
   const [isNotifOpen, setIsNotifOpen] = useState(false);
 
@@ -44,17 +32,12 @@ export function Navbar({ className, mode, userName, openLogin }: NavbarProps) {
 
   const isOnMessagesPage =
     location.pathname.startsWith("/patient-dashboard/messages");
->>>>>>> main
 
   const [currentDateTime, setCurrentDateTime] = useState({
     date: "",
     time: "",
   });
 
-<<<<<<< HEAD
-  const navigate = useNavigate();
-  const userMenuRef = useRef<HTMLDivElement>(null);
-=======
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (messageRef.current && !messageRef.current.contains(event.target as Node)) {
@@ -67,7 +50,6 @@ export function Navbar({ className, mode, userName, openLogin }: NavbarProps) {
     document.addEventListener("mousedown", handleClickOutside);
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
->>>>>>> main
 
   useEffect(() => {
     if (mode === "dashboard") {
@@ -90,52 +72,20 @@ export function Navbar({ className, mode, userName, openLogin }: NavbarProps) {
     }
   }, [mode]);
 
-<<<<<<< HEAD
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        userMenuRef.current &&
-        !userMenuRef.current.contains(event.target as Node)
-      ) {
-        setIsUserMenuOpen(false);
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => document.removeEventListener("mousedown", handleClickOutside);
-  }, []);
-
-=======
->>>>>>> main
   const handleLogout = () => {
     logout();
     navigate("/");
-  };
-
-  const openLogoutConfirm = () => {
-    setIsUserMenuOpen(false);
-    setIsOpen(false);
-    setConfirmLogout(true);
   };
 
   return (
     <nav className={cn('w-full bg-white shadow-md fixed top-0 left-0 z-50', className)}>
       <div className="max-w-8xl mx-auto px-4 h-14 md:h-14 lg:h-16 flex justify-between items-center">
         <div className="flex items-center space-x-4">
-<<<<<<< HEAD
-          <Link to="/" className="flex h-full items-center">
-            <img
-              src={ModalImg.logo3}
-              alt="Logo"
-              className="w-40 h-20 object-contain"
-            />
-=======
           <Link
             to={mode === "dashboard" ? "/patient-dashboard" : "/"}
             className="flex h-full items-center"
           >
             <img src={ModalImg.logo3} alt="Logo" className="w-40 h-20 object-contain" />
->>>>>>> main
           </Link>
 
           {mode === "dashboard" && (
@@ -173,66 +123,6 @@ export function Navbar({ className, mode, userName, openLogin }: NavbarProps) {
             </div>
           )}
 
-<<<<<<< HEAD
-                {isUserMenuOpen && (
-                  <div className="absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-2xl z-50">
-                    <Link
-                      to="/patient-dashboard/profile"
-                      className="block px-6 py-3 hover:bg-gray-100"
-                      onClick={() => setIsUserMenuOpen(false)}
-                    >
-                      Profile
-                    </Link>
-
-                    <div className="border-t border-gray-200 mx-4"></div>
-
-                    <button
-                      onClick={openLogoutConfirm}
-                      className="w-full text-left px-6 py-3 text-red-600 hover:bg-red-50"
-                    >
-                      Logout
-                    </button>
-                  </div>
-                )}
-              </div>
-            ) : null}
-          </div>
-        )}
-        {mode === "dashboard" && (
-          <div className="flex items-center space-x-4 md:space-x-6">
-            <MessageIcon count={99} />
-            <span className="h-6 w-px bg-gray-300"/>
-            <Bell className="w-6 h-6 text-gray-600 cursor-pointer" />
-            <span className="hidden sm:inline">
-              Welcome, <strong>{userName}</strong>!
-            </span>
-            <span className="h-6 w-px bg-gray-300"/>
-
-            <div className="relative" ref={userMenuRef}>
-              <User
-                className="w-8 h-8 text-gray-600 cursor-pointer rounded-full border border-gray-300 p-1"
-                onClick={() => setIsUserMenuOpen(!isUserMenuOpen)}
-              />
-
-              {isUserMenuOpen && (
-                <div className="absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-2xl z-50">
-                  <Link
-                    to="/patient-dashboard/profile"
-                    className="block px-6 py-3 hover:bg-gray-100"
-                    onClick={() => setIsUserMenuOpen(false)}
-                  >
-                    Profile
-                  </Link>
-
-                  <div className="border-t border-gray-200 mx-4"></div>
-
-                  <button
-                    onClick={openLogoutConfirm}
-                    className="w-full text-left px-6 py-3 text-red-600 hover:bg-red-50"
-                  >
-                    Logout
-                  </button>
-=======
           {mode === "dashboard" && (
             <div className="flex items-center space-x-4 md:space-x-6">
               <div className="relative" ref={messageRef}>
@@ -260,7 +150,6 @@ export function Navbar({ className, mode, userName, openLogin }: NavbarProps) {
                   className="cursor-pointer"
                 >
                   <Bell className="w-6 h-6 text-gray-600" />
->>>>>>> main
                 </div>
                   <NotifDropdown
                     notifications={notifications}
@@ -291,66 +180,6 @@ export function Navbar({ className, mode, userName, openLogin }: NavbarProps) {
           </button>
         </div>
       </div>
-<<<<<<< HEAD
-      {isOpen && mode === "dashboard" && (
-        <div className="md:hidden bg-white shadow-md px-6 pb-6 space-y-3">
-          <Link
-            to="/patient-dashboard/profile"
-            className="block px-4 py-2 hover:bg-gray-100"
-            onClick={() => setIsOpen(false)}
-          >
-            Profile
-          </Link>
-
-          <button
-            onClick={openLogoutConfirm}
-            className="w-full text-left px-4 py-2 hover:bg-red-100 text-red-600"
-          >
-            Logout
-          </button>
-        </div>
-      )}
-      <LoginModal
-        isOpen={isLoginOpen}
-        onClose={() => setIsLoginOpen(false)}
-        onForgotPassword={() => {
-          setIsLoginOpen(false);
-          setIsForgotOpen(true);
-        }}
-        onLoginSuccess={(user) => {
-          switch (user.role) {
-            case "Admin":
-              navigate("/admin-dashboard");
-              break;
-            case "Staff":
-              navigate("/staff-dashboard");
-              break;
-            case "Patient":
-              navigate("/patient-dashboard");
-              break;
-            default:
-              navigate("/");
-          }
-          setIsLoginOpen(false);
-        }}
-      />
-
-      <ForgotPassword
-        isOpen={isForgotOpen}
-        onClose={() => setIsForgotOpen(false)}
-      />
-      <ConfirmModal
-        isOpen={confirmLogout}
-        title="Please Confirm"
-        message="Are you sure you want to logout?"
-        confirmText="Logout"
-        onConfirm={() => {
-          setConfirmLogout(false);
-          handleLogout();
-        }}
-        onCancel={() => setConfirmLogout(false)}
-      />
-=======
 
       <div className={`fixed top-0 right-0 h-full w-2/3 bg-white shadow-xl z-40 transform transition-transform duration-300 ${isDrawerOpen ? "translate-x-0" : "translate-x-full"}`}>
         <div className="flex flex-col space-y-4 px-6 mt-16">
@@ -392,7 +221,6 @@ export function Navbar({ className, mode, userName, openLogin }: NavbarProps) {
       />
 
       <ForgotPassword isOpen={isForgotOpen} onClose={() => setIsForgotOpen(false)} />
->>>>>>> main
     </nav>
   );
 }
